98c94cb313e68ccca641988ffe4a25343d0083e3
Weslley Campos <weslley.campos@rebase.com.br>
2025-03-09 23:46:41 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const data = ref(null)
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:3000/teste')
+    data.value = await response.json()
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error)
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>Dados da API</h1>
+    <pre v-if="data">{{ data.message }}</pre>
+    <p v-else>Carregando...</p>
+  </div>
+</template>
