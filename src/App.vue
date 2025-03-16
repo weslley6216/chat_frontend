c4b6057656447ecb4edb6013c69fdff72a06f564
Weslley Campos <weslley.campos@rebase.com.br>
2025-03-16 20:21:29 -0300

    Adjust logout and App.vue components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,75 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-
-const data = ref(null)
-
-onMounted(async () => {
-  try {
-    const response = await fetch('http://localhost:3000/teste')
-    data.value = await response.json()
-  } catch (error) {
-    console.error('Erro ao buscar dados:', error)
-  }
-})
-</script>
-
 <template>
-  <div>
-    <h1>Dados da API</h1>
-    <pre v-if="data">{{ data.message }}</pre>
-    <p v-else>Carregando...</p>
+  <div id="app">
+    <header>
+      <h1>Web Chat</h1>
+    </header>
+
+    <main>
+      <p v-if="userStore.username" class="welcome-message">
+        Welcome, {{ userStore.username }}!
+        <button @click="logout">Logout</button>
+      </p>
+      <router-view />
+    </main>
   </div>
 </template>
+
+<script>
+import { useUserStore } from '@/stores/user';
+import { logoutUser } from '@/services/auth';
+
+export default {
+  name: 'App',
+  computed: {
+    userStore() {
+      const store = useUserStore();
+      console.log('userStore:', store);
+      return store;
+    },
+  },
+  methods: {
+    logout() {
+      logoutUser();
+      this.userStore.clearUser();
+      this.$router.push('/');
+    },
+  },
+  created() {
+    this.userStore.loadUserFromStorage();
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Arial', sans-serif;
+  text-align: center;
+}
+
+header {
+  background-color: #616774;
+  color: white;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 20px;
+  font-size: 25px;
+}
+
+footer {
+  background-color: #282c34;
+  color: white;
+  padding: 10px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+main {
+  margin: 20px;
+}
+
+.welcome-message {
+  text-align: right;
+  margin-right: 20px;
+}
+</style>
---
+++ src/components/Logout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <button @click="logout">Logout</button>
+    </div>
+  </template>
+  
+  <script>
+  import { useUserStore } from '@/stores/user';
+  
+  export default {
+    methods: {
+      logout() {
+        const userStore = useUserStore();
+        userStore.clearUser();
+  
+        localStorage.removeItem('authToken');
+  
+        this.$router.push('/login');
+      },
+    },
+  };
+  </script>
+  
